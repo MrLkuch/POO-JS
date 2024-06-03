d5a3a77715aeceb0b62f735e062b091a54177fe3
MrLkuch <kucheida.lucas@gmail.com>
2024-06-03 14:40:25 +0200

    feat(02-encapsulation.js): complete exercise

---
+++ src/02-encapsulation.js
@@ -0,0 +1,55 @@
+'use strict'
+
+class car {
+    #brand = '';
+    #model = '';
+    #year = '';
+    #color = '';
+
+    constructor(brand, model, year, color) {
+        this.#brand = brand;
+        this.#model = model;
+        this.#year = year;
+        this.#color = color;
+    }
+
+    setBrand(value) {
+        this.#brand = value;
+    }
+
+    getBrand() {
+        return this.#brand;
+    }
+
+    setModel(value) {
+        this.#model = value;
+    }
+
+    getModel() {
+        return this.#model;
+    }
+
+    setYear(value) {
+        this.#year = value;
+    }
+
+    getYear() {
+        return this.#year;
+    }
+
+    setColor(value) {
+        this.#color = value;
+    }
+
+    getColor() {
+        return this.#color;
+    }
+
+    showDetails() {
+        console.log(this.#brand, this.#model, this.#year, this.#color)
+    }
+}
+
+let renaudR5 = new car('renaud', 'R5', '2024', 'black');
+renaudR5.setColor('white');
+console.log(renaudR5.getColor());
