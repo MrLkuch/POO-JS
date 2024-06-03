f444133a82b81eae2a52c5fefc3a7e954335ab39
MrLkuch <kucheida.lucas@gmail.com>
2024-06-03 14:58:39 +0200

    feat(03-inheritance.js): complete exercice

---
+++ src/03-inheritance.js
@@ -0,0 +1,22 @@
+class Vehicle {
+    constructor(brand, model, year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+}
+
+class Car extends Vehicle {
+    constructor(brand, model, year, color) {
+        super(brand, model, year);
+
+        this.color = color;
+    }
+
+    showDetails() {
+        console.log(this.brand, this.model, this.year, this.color)
+    }
+}
+
+let renaudR5 = new Car('renaud', 'R5', '2024', 'black');
+renaudR5.showDetails();
